9a6291af40819447d42cb50e34a30e1445ee397d
quenii <quenii@e4c55939-9875-e5a8-f322-dd768a204feb>
2011-11-02 14:39:09 +0000

    New INL/DNL algorithm

--- soft/3rdparty/m2c/m/inldnl.m
+++ soft/3rdparty/m2c/m/inldnl.m
@@ -1,87 +1,150 @@
-%Code density/histofgram test to calculate INL and DNL require a large number of samples.
-%Step 1: Apply a close to full-scale sine wave (but not clipping) and find the mid-code for the applied signal.
-%Step 2: Apply the same sine wave input, but slightly larger amplitude to clip the ADC slightly.
-%Run the following program, enter the number of samples, resolution and mid-code from Step 1and continue.
-%Copyright Au/Hofner, Maxim Integrated Products, 120 San Gabriel Drive, Sunnyvale, CA94086
-%This program is believed to be accurate and reliable. This program may get altered without prior notification.
+% $$$ clear all;
+% $$$ 
+% $$$ filename=input('Enter File Name: ','s');    %enter the file name of the datas you acquired from the ADC, for example:file2.txt
+% $$$ 
+% $$$ fid=fopen(filename,'r');
+% $$$ if fid==-1,
+% $$$    warning='no such file'
+% $$$    return;
+% $$$ end
+% $$$ 
+% $$$ numpt=input('Enter Number of Data Points:  ');   %for example of file2.txt:8388608
+% $$$ numbit=input ('Enter ADC Resolution:  ');        %for example of file2.txt:14
+% $$$ 
+% $$$ T1=input('T1:');    %the transition level of logic 0
+% $$$ T2=input('T2:');    %the transition level of logic 2^numbit
+% $$$ 
+% $$$ T_ideal_1=input('T_ideal_1:');   %the ideal level of logic 0
+% $$$ T_ideal_2=input('T_ideal_2:');   %the ideal level of logic 2^numbit
+% $$$ 
+% $$$ 
+% $$$ [v1,count]=fscanf(fid,'%f',[1,numpt]);
+% $$$ fclose(fid);
 
-filename=input('Enter File Name: ');
-if isempty(filename)
-   filename = 'listing';
+function [INLar, DNLar, H, indexl, indexh] = inldnl(samples, numbit, numpt, T1, T2, T_ideal_1, T_ideal_2)
+% INLDNL - 
+% plot([(indexl):(indexh-2)],DNLar);
+% plot([(indexl):(indexh-1)],INLar);
+
+NCODES = 2^numbit;
+
+v1 = samples;
+v1 = v1';
+v2 = v1/2^(16-numbit)+2^(numbit-1);
+code = round(v2(:,1)-0.4999999);
+
+H = zeros(1,NCODES);
+Hc = zeros(1,NCODES-1);
+T = zeros(1,NCODES-1);
+Tnom = zeros(1,NCODES-1);
+S = numpt;
+
+
+for i = 1:size(code),
+    H(code(i)+1) = H(code(i)+1)+1;
 end
-fid=fopen(filename,'r');
-numpt=input('Enter Number of Data Points:  ');
-numbit=input ('Enter ADC Resolution:  ');
-mid_code=input('Enter Mid-Code (Mean Code):  ');
 
-% for i=1:13,	
-%    fgetl(fid);
-% end
-[v1,count]=fscanf(fid,'%f',[1,numpt]);
-fclose(fid);
 
-code=v1';
-code = fix( code / 2^( 16 - numbit ) );
-code = code + 2^(numbit-1);
-code_count=zeros(1,2^numbit);
-
-for i=1:size(code),
-   code_count(code(i)+1)=code_count(code(i)+1) + 1;
+%======================================
+%search for the positions of '111бн11' and '000бн00'
+%======================================
+for jh = 2^(numbit-1):NCODES;
+    if H(jh) == 0;
+        indexh = jh-1;
+        break;
+    else 
+        indexh = jh;
+    end
 end
 
-if code_count(1) == 0 | code_count(2^numbit) == 0 | ...
-  (code_count(1) < code_count(2)) | (code_count(2^numbit-1) > code_count(2^numbit))
-   disp('ADC not clipping ... Increase sinewave amplitude!');
-%    break;
+for jl = 2^(numbit-1):-1:1;
+    if H(jl)==0;
+        indexl = jl+1;
+        break;
+    else 
+        indexl = jl;
+    end
 end
 
-A=max(mid_code,2^numbit-1-mid_code)+0.1; 
-vin=(0:2^numbit-1)-mid_code;	
-sin2ramp=1./(pi*sqrt(A^2*ones(size(vin))-vin.*vin));
-
-while sum(code_count(2:2^numbit-1)) < numpt*sum(sin2ramp(2:2^numbit-1))
-  A=A+0.1;
-  sin2ramp=1./(pi*sqrt(A^2*ones(size(vin))-vin.*vin));
+if indexh<NCODES & indexl>1
+    warning = 'can not reach "111..11" and "000..00"';
+    return;
+elseif indexl>1
+    warning = 'can not reach "000..00"';
+    return;
+elseif indexh<NCODES
+    warning = 'can not reach "111..11"';
+    return;
 end
 
-disp('You Have Applied a Sine Wave of (dBFS): '); 
-Amplitude=A/(2^numbit/2)
+for k = 1:(NCODES-1),
+    Tnom(k) = (k-0.5)*(T_ideal_2-T_ideal_1)/(NCODES);
+    for i = 1:k,
+        Hc(k) = Hc(k)+H(i);
+    end
+end
+
+Q = (Tnom(NCODES-1)-Tnom(1))/(NCODES-2);
+A = (T2-T1)/(cos(pi*Hc(1)/S)+cos(pi*(1-(Hc(NCODES-1)/S))));
+C = (T2*cos(pi*Hc(1)/S)+T1*cos(pi*(1-(Hc(NCODES-1)/S))))/(cos(pi*Hc(1)/S)+cos(pi*(1-(Hc(NCODES-1)/S))));
+
+
+for k = 1:(NCODES-1),
+    T(k) = C-A*cos(pi*Hc(k)/S);
+end
+
+%==============
+%best fit
+%==============   
+%in this file2.txt example, the best fit is better than end point
+sTk = 0;
+sT = 0;
+sT2 = 0;
+
+for k = 1:(NCODES-1),
+    sTk = sTk+k*T(k);
+    sT = sT+T(k);
+    sT2 = sT2+T(k)*T(k);
+end
+
+G = Q*(NCODES-1)*(sTk-2^(numbit-1)*sT)/((NCODES-1)*sT2-sT*sT);
+Vos = Tnom(1)+Q*(2^(numbit-1)-1)-G*sT/(NCODES-1);
+
+%==============
+%calculate INL
+%==============
+for k = (indexl):(indexh-1),
+    e(k) = Tnom(k)-G*T(k)-Vos;
+    INLar(k) = e(k)/Q;
+end
+
+%==============
+%calculate DNL
+%==============
+for k = (indexl):(indexh-2),
+    DNLar(k) = G*(T(k+1)-T(k))/Q-1;
+end
+
+
+%==============
+%plot figures
+%==============
 figure;
-plot([0:2^numbit-1],code_count,[0:2^numbit-1],sin2ramp*numpt);
-title('CODE HISTOGRAM - SINE WAVE');
-xlabel('DIGITAL OUTPUT CODE');
-ylabel('COUNTS');
-% axis([0 2^numbit-1 0 max(code_count(2),code_count(2^numbit-1))]);
-code_countn=code_count(2:2^numbit-1)./(numpt*sin2ramp(2:2^numbit-1)); 
+plot([1:NCODES],H);
+axis([0 NCODES 0 max(H)]);
+
 figure;
-plot([1:2^numbit-2],code_countn);
-title('CODE HISTOGRAM - NORMALIZED')
-xlabel('DIGITAL OUTPUT CODE');
-ylabel('NORMALIZED COUNTS');
-
-dnl=code_countn-1;
-inl=zeros(size(dnl));
-for j=1:size(inl')
-   inl(j)=sum(dnl(1:j));
-end
-
-%End-Point fit INL
-%[p,S]=polyfit([1,2^numbit-2],[inl(1),inl(2^numbit-2)],1);
-
-%Best-straight-line fit INL
-[p,S]=polyfit([1:2^numbit-2],inl,1);
-inl=inl-p(1)*[1:2^numbit-2]-p(2);
-
-disp('End Points Eliminated for DNL and INL Calculations');
-figure;
-plot([1:2^numbit-2],dnl);
+plot([(indexl):(indexh-2)],DNLar);
+axis([0 NCODES -max(abs(DNLar)) max(abs(DNLar))]);
 grid on;
 title('DIFFERENTIAL NONLINEARITY vs. DIGITAL OUTPUT CODE');
 xlabel('DIGITAL OUTPUT CODE');
 ylabel('DNL (LSB)');
+
 figure;
-plot([1:2^numbit-2],inl);
+plot([(indexl):(indexh-1)],INLar);
+axis([0 NCODES -max(abs(INLar)) max(abs(INLar))]);
 grid on;
 title('INTEGRAL NONLINEARITY vs. DIGITAL OUTPUT CODE');
 xlabel('DIGITAL OUTPUT CODE');
-ylabel('INL(LSB)');  
+ylabel('INL(LSB)');
